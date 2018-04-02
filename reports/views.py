9f35f229e24224080aa65dad45572ef2dcb9cc69
ken <tsuruta1704@gmail.com>
2018-04-03 01:01:45 +0900

    Commit

--- reports/views.py
+++ reports/views.py
@@ -6,11 +6,12 @@ from .models import Report, Company
 # Create your views here.
 
 def index(request):
-    reports = Report.objects.filter(type_of_current_period='FY')
+    reports = Report.objects.filter(type_of_current_period='FY')[:100]
     return render(request, 'reports/index.html', {'reports':reports})
 
 def detail(request, edinet_code):
     company = Company.objects.get(edinet_code=edinet_code)
-    reports = Report.objects.filter(edinet_code=edinet_code)
-    return render(request,'reports/detail.html', {'company': company, 'reports':reports})
+    latest_report = Report.objects.filter(edinet_code=edinet_code, type_of_current_period='FY').order_by('-year')[0]
+    reports = Report.objects.filter(edinet_code=edinet_code, type_of_current_period='FY')
+    return render(request,'reports/detail.html', {'company': company, 'reports':reports, 'latest_report':latest_report })
 
