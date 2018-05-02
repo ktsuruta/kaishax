88954ce7f00755cf17c4ba2d98a38a8d47b6e479
鶴田健 <tsurutaken@tsurutakennoMacBook-puro.local>
2018-05-02 15:50:28 +0900

    Add common

--- kaishax/urls.py
+++ kaishax/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('common.urls')),
     path('reports/', include('reports.urls')),
     path('admin/', admin.site.urls),
 ]
